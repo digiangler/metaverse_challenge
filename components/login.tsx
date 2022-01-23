0317c6b235f5317f9eda6f74fe55730a808fa4e0
Hiroshi Yamamoto <digiangler777@gmail.com>
2022-01-23 17:01:40 +0900

    🚀 login flow with Moralis completed.

--- components/login.tsx
+++ components/login.tsx
@@ -1,20 +1,24 @@
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 function login() {
+  const { authenticate } = useMoralis();
+
   return (
     <div className="relative bg-black">
-      <h1>I am the login screen</h1>
+      <h1>I am the login</h1>
       <div className="absolute z-50 flex flex-col items-center justify-center w-full space-y-4 h-4/6">
-        {/* Digi-Angler logo */}
         <Image
           className="object-cover rounded-full"
           src="https://github.com/digiangler.png"
           height={200}
           width={200}
         />
-        {/* Login button */}
-        <button className="p-5 font-bold bg-yellow-500 rounded-lg animate-pulse">
-          Login yo the Metaverse
+        <button
+          onClick={authenticate}
+          className="bg-orange-200 rounded-lg p-5 font-bold animate-pulse "
+        >
+          Login to the METAVERSE
         </button>
       </div>
       <div className="w-full h-screen">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { MoralisProvider } from "react-moralis";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const app = process.env.NEXT_PUBLIC_APP_ID as string;
+  const url = process.env.NEXT_PUBLIC_SERVER_URL as string;
+
   return (
-    <MoralisProvider
-      appId={process.env.NEXT_PUBLIC_APP_ID}
-      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
-    >
+    <MoralisProvider appId={app} serverUrl={url}>
       <Component {...pageProps} />
     </MoralisProvider>
   );
